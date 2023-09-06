66112a903ec8eff1f993d4a734e554f635fda566
Abubakar Siddique <abz.siddique@gmail.com>
2023-09-06 12:04:21 +0100

    feat: setup the apollo client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+/*
+  This enables it to store and reuse query results so it doesn't have to make as many 
+  network requests. This makes our app's user experience feel much snappier.
+*/
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
+
   <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
