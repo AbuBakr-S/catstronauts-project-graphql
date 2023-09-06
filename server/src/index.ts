b50c3e51689d09774acbfe93088e10738fb21ac1
Abubakar Siddique <abz.siddique@gmail.com>
2023-09-06 18:10:39 +0100

    feat: replace mock data with live data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,50 +1,32 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema";
-
-import { addMocksToSchema } from "@graphql-tools/mock";
-import { makeExecutableSchema } from "@graphql-tools/schema";
-
-/*
-  With mocks enabled, Apollo Server always returns exactly two entries for every list field.
-  To get more entries at a time, let's say 6, we'll add a Query.tracksForHome to our mocks 
-  object and return an Array of that given length like so: [...new Array(6)].
-*/
-const mocks = {
-  Query: () => ({
-    tracksForHome: () => [...new Array(6)],
-  }),
-  Track: () => ({
-    id: () => "track_01",
-    title: () => "Astro Kitty, Space Explorer",
-    author: () => {
-      return {
-        name: "Grumpy Cat",
-        photo:
-          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
-      };
-    },
-    thumbnail: () =>
-      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
+import { resolvers } from "./resolvers";
+// our data source file (extending RESTDataSource),
+import { TrackAPI } from "./datasources/track-api";
 
 const startApolloServer = async () => {
   // Pass in our typeDefs into its options object
-  const server = new ApolloServer({
-    /*
-      With this code, we're generating an executable schema from our typeDefs,
-      and instructing Apollo Server to populate every queried schema field 
-      with a placeholder value (such as Hello World for String fields).
-    */
-    schema: addMocksToSchema({
-      schema: makeExecutableSchema({ typeDefs }),
-      mocks
-    })
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  /* 
+    Our resolver functions expect to find dataSources.trackAPI on their contextValue, 
+    which is why we've defined a property called dataSources here in our server.
+    dataSources name is a convention.
+  */
+
+  const { url } =  await startStandaloneServer(server, {
+    // context function that returns an object that all our resolvers will share
+    context: async () => {
+      // access caching capabilities
+      const { cache } = server;
+      return {
+        dataSources: {
+          trackAPI:  new TrackAPI({ cache }),
+        },
+      };
+    },
   });
-  const { url } =  await startStandaloneServer(server);
   console.log(`
     🚀  Server is running!
     📭  Query at ${url}
--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -5,19 +5,18 @@ export const resolvers = {
     // the homepage grid of our web client
     // ? Destructure dataSources from contextValue
     tracksForHome: (_, __, { dataSources }) => {
-      return dataSources.trackAPI.getTracksForHome()
+      return dataSources.trackAPI.getTracksForHome();
     },
     // ? Another resolver specifically for a track's author
     // The Track key indicates it's for the Track type in our schema
     // Inside that Track key will be another object with an author field, where we'll define our resolver.
-    Track: {
-      author: ({ authorId }, _, { dataSources }) => {
-        // getAuthor needs an authorId which we can access from the parent arg returned by tracksForHome
-        // tracksForHome returns a list and calls the author resolver once for each track
-        // It passes the current track as the value of parent, enabling us to extract the authorId.
-        console.log(parent)
-        return dataSources.trackAPI.getAuthor(authorId)
-      },
+  },
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      // getAuthor needs an authorId which we can access from the parent arg returned by tracksForHome
+      // tracksForHome returns a list and calls the author resolver once for each track
+      // It passes the current track as the value of parent, enabling us to extract the authorId.
+      return dataSources.trackAPI.getAuthor(authorId)
     },
-  }
+  },
 };
