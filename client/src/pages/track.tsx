3aede887d8424147297ec31b825436bdfabd1c38
Abubakar Siddique <abz.siddique@gmail.com>
2023-09-08 17:40:31 +0100

    feat: initialise track page

---
+++ client/src/pages/track.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { gql } from "../__generated__";
+import { useQuery } from "@apollo/client";
+import { Layout, QueryResult } from "../components";
+import { useParams } from "react-router-dom";
+
+const TRACK = gql(`
+  query Track($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      description
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+    }
+  }
+`)
+
+export const Track = () => {
+  const { trackId } = useParams();
+  console.log(trackId)
+  return (
+    <Layout></Layout>
+  )
+}
